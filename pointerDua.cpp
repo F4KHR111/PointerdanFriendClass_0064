480e709462954a313b4362035ac73df02caa08f4
F4KHR111 <fakhriwirdiyan05@gmail.com>
2024-05-06 15:10:10 +0700

    Menambahkan akses modifier public, menambahkan member nim serta metode show nim

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa{
-
+class mahasiswa
+{
+public:
+    int nim;
+    void showNim(){
+        cout << "No Induk = " << nim << endl;
+    }    
 };
